f2c4bd302cee7fdfb8d7aaf69ea510916aa1d5ba
nicolla-s <shikwambananicolla@gmail.com>
2025-06-17 09:30:42 +0200

    add api
    added my api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "4a6374fa1fb0905ff44eb3o0787f64at";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
